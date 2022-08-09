767c8a0cf6f6d3d66c7ec3ef4299a17e355798f1
Manu <manuel.chavez@accenture.com>
2022-08-09 09:22:55 -0500

    adding setting page and controls

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,18 +1,31 @@
-﻿namespace MauiApp1;
+﻿using System.Windows.Input;
+
+namespace MauiApp1;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
-		//Routing.RegisterRoute("Settings", typeof(Views.Settings));
+		Routing.RegisterRoute("Settings", typeof(Views.Settings));
 		Routing.RegisterRoute("Detail", typeof(Views.Detail));
 
 		InitializeComponent();
 
-		//SettingsCommand = new Command(async () =>
-		//{
-		//	await AppShell.Current.GoToAsync("Settings");
-		//});
+		SettingsCommand = new Command(async () =>
+		{
+			await AppShell.Current.GoToAsync("Settings");
+		});
 		this.BindingContext = this;
+
+
+		#if ANDROID
+				this.Title = this.Title + "-Android";
+		#elif IOS
+				this.Title = this.Title + "-ios";
+		#elif WINDOWS
+				this.Title = this.Title + "-WinUI";
+		#endif
 	}
+
+	public ICommand SettingsCommand { get; }
 }
---
+++ Views/Settings.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiApp1.Views;
+
+public partial class Settings : ContentPage
+{
+	public Settings()
+	{
+		InitializeComponent();
+		SelectedDate = DateTime.Now;
+		BindingContext = this;
+	}
+
+	public DateTime SelectedDate { get; set; }
+
+}
+
